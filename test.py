ce3dd0188dc3368ced1dc29d6c5f2bbda6bbf274
han0nly <zhangxh@stu.xidian.edu.cn>
2021-01-29 15:52:20 +0800

    update

--- test.py
+++ test.py
@@ -15,16 +15,16 @@ import fire
 import os
 
 
-def find_and_move(profiles: str, apk_files: str, dest_dir: str) -> (list, list):
+def find_and_move(profiles: str, apk_files: str, dest_dir: str) -> list:
     app_profiles = {}
     for root, dirs, files in os.walk(profiles):
         for name in files:
-            # 将文件名逐个放入q
             # app_profiles.append(root + '/' + name)
             if 'json' == name.split('.')[-1]:
                 with open(root + '/' + name, 'r') as f:
                     profile = json.load(f)
-                    if 'lib_matches' in profile.keys() and len(profile['lib_matches']) > 0:
+                    print(profile.keys())
+                    if ('lib_matches' in profile.keys()) and (len(profile['lib_matches']) > 0):
                         app_profiles[profile['appInfo']['fileName']] = []
                         used_lib = []
                         for lib in profile['lib_matches']:
@@ -33,15 +33,12 @@ def find_and_move(profiles: str, apk_files: str, dest_dir: str) -> (list, list):
     matched_apps = []
     for root, dirs, files in os.walk(apk_files):
         for name in files:
-            # 将文件名逐个放入q
             if name[:-4] in app_profiles.keys():
                 matched_apps.append(root + '/' + name)
                 shutil.move(root + '/' + name, dest_dir + '/' + name)
-
     return matched_apps
 
 
-
 if __name__ == '__main__':
-    fire.Fire()
+    fire.Fire(find_and_move)
     # print(app_profiles)
